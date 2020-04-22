da5a997e7f40fcff0ca39531a8e5370e060a0b4d
bb35319008 <bb35319008@ms.nagasaki-u.ac.jp>
2020-04-22 23:49:28 +0900

    4月22日　#1回目

--- k01/k01.c
+++ k01/k01.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave);
+extern double var_online(double val,double ave,double square_ave);
+int n = 1;
+double ave = 0, val, s, A, S, square_ave;
 
 int main(void)
 {
-    double val;
+    double u;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -26,12 +28,9 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+        A = ave_online(val, ave);
+        S = var_online(val, ave, square_ave);
+        n++;
     }
 
     if(fclose(fp) == EOF){
@@ -39,9 +38,27 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    u = S * n / (n-1);
+
+    printf("Sample's average is %.2f, Sample's variance is %.2f.\n", A, S);
+    printf("Population's average is %.2f, Population's variance is %.2f.\n", A, u);
 
     return 0;
-
-
 }
 
+extern double ave_online(double val,double ave)
+{
+    ave = ave * (n - 1)/n + val / n;
+    return ave;
+}
+
+extern double var_online(double val,double ave,double square_ave)
+{
+    A = ave_online(val, ave);
+
+    square_ave = square_ave * (n-1) / n + (double)pow(val, 2 / n);
+    
+    s = square_ave - (double)pow(A, 2);
+
+    return s;
+}
