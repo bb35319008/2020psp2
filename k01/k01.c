439ee9d279a06aad4a0e644eadf2659494d77d30
bb35319008 <bb35319008@ms.nagasaki-u.ac.jp>
2020-07-22 12:40:02 +0900

    7月22日　#4回目

--- k01/k01.c
+++ k01/k01.c
@@ -5,11 +5,11 @@
 
 extern double ave_online(double val,double ave);
 extern double var_online(double val,double ave,double square_ave);
-int n = 1;
 double S, ave, square_ave;
 
 int main(void)
 {
+    int N=0;
     double u, val;
     char fname[FILENAME_MAX];
     char buf[256];
@@ -25,20 +25,22 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
+    square_ave = 1;
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
         ave = ave_online(val, ave);
+        printf("square_ave1= %lf\n", square_ave);
         S = var_online(val, ave, square_ave);
-        n++;
+        printf("square_ave = %lf\n", square_ave);
+        N++;
     }
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
-    u = S * n / (n-1);
+    
+    u = S * N / (N-1);
 
     printf("sample mean : %.2lf\n", ave);
     printf("sample variance : %.2lf\n", S);
@@ -50,22 +52,28 @@ int main(void)
 
 extern double ave_online(double val,double ave)
 {
+    static int n=1; 
     double a=ave;
 
     a = ave * (n - 1)/n + val / n;
-
+    n++;
+    printf("%d, %lf\n", (n-1)/n, a);
     return a;
 }
 
 extern double var_online(double val,double ave,double square_ave)
 {
-    double s, aa=ave, z=square_ave;
+    static double nn=1;
+    double s, z=square_ave, v;
+    printf("square_ave2 = %lf", square_ave);
+    z = square_ave * square_ave * (nn-1) / nn;
+    v = val * val /nn;
 
-    aa = ave * (n-1)/n + val/n;
-
-    z = square_ave * square_ave * (n-1) / n + val * val / n;
+    square_ave = z + v;
     
-    s = z - aa * aa;
-    printf("%lf, %lf, %lf, %lf\n", z, aa, square_ave, s);
+    s = square_ave - ave * ave;
+    nn++;
+    printf("%lf, %lf, %lf, %lf, %lf, %lf\n", nn, z, v, (nn-1)/nn, square_ave, s);
+    
     return s;
 }
